33b70c010f241c2bc93d0aa2db69b0038ed04924
Javanseal <agomjavan@gmail.com>
2024-01-04 14:47:45 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,43 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * print_number -Prints an integer
+ *
+ * @n: input integer
+ *
+ * Return: void
+ */
 
-int main(void)
+void print_number(int n)
 {
-int n;
-int a[5];
-int *p;
 
-a[2] = 1024;
-p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   *You can only use _putchar function to print
-   *You are not allowed to use long
-   *You are not allowed to use arrays or pointers
-   *You are not allowed to hard-code special values
-   */
-p[5] = 98;
-/* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
-return (0);
+	int len, index;
+
+	len = 1000000000;
+
+	if (n < 0)
+	{
+		_putchar('-');
+
+	if (n == -2147483648)
+	{
+		index = 1;
+		n += 1;
+	}
+	n = -n;
+	}
+	while (len != 1)
+	{
+		if (n >= len)
+			_putchar((n / len) % 10 + '0');
+		len /= 10;
+	}
+	if (index == 1)
+	{
+		_putchar(((n % 10) + 1) + '0');
+	}
+	else
+	{
+		_putchar(n % 10 + '0');
+	}
 }
+
