60e772f152241aeeb328c249b3e5f259c74b0844
Javanseal <agomjavan@gmail.com>
2024-01-17 09:42:27 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,30 +1,47 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * *_calloc - function to allocates memory
- * @nmemb: unsigned int type
- * @size: unsigned int type
- * Return: return pointer to array
+ * str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: a pointer to a newly allocated space in memory which
+ * contains the contents of s1, followed by the contents of s2,
+ * and null terminated. NULL on failure
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *str_concat(char *s1, char *s2)
 {
-	char *ptr;
-	unsigned int count;
+	int i, j, len1, len2, len;
+	char *result;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
+	len1 = len2 = 0;
+
+	if (s1 != NULL)
 	{
-		return (NULL);
+		i = 0;
+		while (s1[i++] != '\0')
+			len1++;
 	}
-	count = 0;
-	while (count < nmemb * size)
+
+	if (s2 != NULL)
 	{
-		ptr[count] = 0;
-		count++;
+		i = 0;
+		while (s2[i++] != '\0')
+			len2++;
 	}
-	return (ptr);
+
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		result[i] = s2[j];
+	result[len] = '\0';
+
+	return (result);
 }
